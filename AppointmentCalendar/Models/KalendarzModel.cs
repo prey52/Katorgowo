ef299f113a91ee9d4f495cac5b8815cdc3e78c14
piki13 <paulinakozuch@gmail.com>
2024-05-30 19:29:06 +0200

    Dodanie modeli

---
+++ AppointmentCalendar/Models/KalendarzModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentCalendar.Models
+{
+    public class KalendarzModel
+    {
+        public int IdRekrutera { get; set; }
+        public List<DateTime> WolneTerminy { get; set; }
+    }
+}
---
+++ AppointmentCalendar/Models/WydarzeniaModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentCalendar.Models
+{
+
+    public class WydarzeniaModel
+    {
+        public int IdWydarzenia { get; set; }
+        public int IdAplikacji { get; set; }
+        public int IdRekrutera { get; set; }
+        public DateTime DataSpotkania { get; set; }
+    }
+}
